2e70fb23bc1f838868577d86a04ced81fb242fde
Syazwan Zamree <syazwanzamree@gmail.com>
2018-08-03 11:13:30 +0800

    create team resolvers: get, create, delete etc. #9

--- src/api/graphql/resolvers/teamResolvers.js
+++ src/api/graphql/resolvers/teamResolvers.js
@@ -1,25 +1,3 @@
-class Team {
-  constructor({
-    displayName,
-    name,
-    description,
-    avatar,
-    memberList,
-    projectList,
-    createdAt,
-    createdBy,
-  }) {
-    this.displayName = displayName;
-    this.name = name;
-    this.description = description;
-    this.avatar = avatar;
-    this.memberList = memberList;
-    this.projectList = projectList;
-    this.createdAt = createdAt;
-    this.createdBy = createdBy;
-  }
-}
-
 const teamResolvers = {
   Query: {
     getTeams: async (parent, args, { models }) => {
@@ -39,9 +17,51 @@ const teamResolvers = {
     },
   },
   Mutation: {
-    addTeam: (parent, { input }) => {
-      const newTeam = new Team(input);
-      return newTeam;
+    createTeam: async (parent, { input }, { models }) => {
+      const { displayName: inputDisplayName } = input;
+
+      if ((await models.Team.find({ displayName: inputDisplayName })).length) throw new Error('taken displayName');
+
+      const team = new models.Team(input)
+        .save()
+        .then(d => d)
+        .catch(e => console.log('error', e));
+
+      return team;
+    },
+    updateTeam: async (parent, { id, input }, { models }) => {
+      const { displayName: inputDisplayName } = input;
+
+      if (!(await models.Team.findById(id))) throw new Error('no such id in db');
+
+      const takenProps = await models.User.find({
+        $and: [{ displayName: inputDisplayName }, { $nor: [await models.Team.findById(id)] }],
+      });
+
+      if (takenProps.length) throw new Error('display name is taken');
+
+      const checkError = (e) => {
+        if (e) throw new Error('cannot update team');
+      };
+
+      const team = await models.Team.findByIdAndUpdate(
+        id,
+        { $set: input },
+        e => checkError(e),
+      )
+        .then(d => d)
+        .catch(e => console.log('e', e));
+
+      return team;
+    },
+    deleteTeam: async (parent, { id }, { models }) => {
+      if (!(await models.Team.findById(id))) throw new Error('no such id in db');
+
+      const team = await models.Team.findByIdAndRemove(id)
+        .then(d => d)
+        .catch(e => console.log('e', e));
+
+      return team;
     },
   },
 };
