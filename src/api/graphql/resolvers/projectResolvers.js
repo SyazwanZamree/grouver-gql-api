87f210d2d5dfe76fe3fe2957bf6fd8daf16e9389
Syazwan Zamree <syazwanzamree@gmail.com>
2018-08-03 13:09:19 +0800

    add project resolvers for create, update, delete etc. #10

--- src/api/graphql/resolvers/projectResolvers.js
+++ src/api/graphql/resolvers/projectResolvers.js
@@ -1,36 +1,67 @@
-class Project {
-  constructor({
-    displayName,
-    name,
-    description,
-    avatar,
-    memberList,
-    taskList,
-    createdAt,
-    createdBy,
-  }) {
-    this.displayName = displayName;
-    this.name = name;
-    this.description = description;
-    this.avatar = avatar;
-    this.memberList = memberList;
-    this.taskList = taskList;
-    this.createdAt = createdAt;
-    this.createdBy = createdBy;
-  }
-}
-
 const projectResolvers = {
   Query: {
-    getProjects: (parent) => {
-      console.log('parent', parent);
-      return parent;
+    getProjects: async (parent, args, { models }) => {
+      const projects = await models.Project.find()
+        .then(d => d)
+        .catch(e => console.log('e: ', e));
+
+      return projects;
+    },
+    getProject: async (parent, { id }, { models }) => {
+      if (!(await models.Project.findById(id))) throw new Error('no such id in db');
+
+      const project = await models.Project.findById(id)
+        .then(d => d)
+        .catch(e => console.log('e: ', e));
+
+      return project;
     },
   },
   Mutation: {
-    addProject: (parent, { input }) => {
-      const newProject = new Project(input);
-      return newProject;
+    createProject: async (parent, { input }, { models }) => {
+      const { displayName: inputDisplayName } = input;
+
+      if ((await models.Project.find({ displayName: inputDisplayName })).length) throw new Error('taken displayName');
+
+      const project = new models.Project(input)
+        .save()
+        .then(d => d)
+        .catch(e => console.log('error', e));
+
+      return project;
+    },
+    updateProject: async (parent, { id, input }, { models }) => {
+      if (!(await models.Project.findById(id))) throw new Error('no such id in db');
+
+      const { displayName: inputDisplayName } = input;
+      const takenProps = await models.Project.find({
+        $and: [{ displayName: inputDisplayName }, { $nor: [await models.Project.findById(id)] }],
+      });
+
+      if (takenProps.length) throw new Error('display name is taken');
+
+      const checkError = (e) => {
+        if (e) throw new Error('cannot update team');
+      };
+
+      const project = await models.Project.findByIdAndUpdate(
+        id,
+        { $set: input },
+        e => checkError(e),
+      )
+        .then(d => d)
+        .catch(e => console.log('e', e));
+
+      return project;
+    },
+    deleteProject: async (parent, { id }, { models }) => {
+      if (!(await models.Project.findById(id))) throw new Error('no such id in db');
+
+      const project = await models.Project.findByIdAndRemove(id)
+        .then(d => d)
+        .catch(e => console.log('e', e));
+
+      return project;
     },
   },
 };
