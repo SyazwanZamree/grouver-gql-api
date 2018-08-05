79c2887cd75da01c17d223ebf8fedc736e6b067b
Syazwan Zamree <syazwanzamree@gmail.com>
2018-08-05 11:27:46 +0800

    include req in context. #12

--- src/api/graphql/index.js
+++ src/api/graphql/index.js
@@ -3,10 +3,11 @@ import resolvers from './resolvers/index';
 import typeDefs from './schemas/index';
 import models from './../database/models/index';
 
-const context = {
+const context = req => ({
   models,
+  req,
   db: 'this is db setup',
-};
+});
 
 const gqlserver = new GraphQLServer({
   typeDefs,
