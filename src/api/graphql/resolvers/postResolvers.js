a4c84e877fe2d4462d8dffff31659c12deb6e82e
Syazwan Zamree <syazwanzamree@gmail.com>
2018-10-18 21:11:02 +0800

    create getPosts resolvers. #15

--- src/api/graphql/resolvers/postResolvers.js
+++ src/api/graphql/resolvers/postResolvers.js
@@ -1,27 +1,45 @@
 const postResolvers = {
-  Post: {
-    __resolveType: async (parent) => {
-      if (parent.assignedTo) {
-        console.log('ya1');
-        return 'Task';
-      }
-
-      if (parent.replies) {
-        console.log('ya2');
-        return 'Discussion';
-      }
-
-      return null;
-    },
-  },
   Query: {
-    getPosts: async () => {
-      console.log('Holla');
+    getPosts: async (parent, args, { models, userSession, projectSession }) => {
+      if (!projectSession || userSession.invalidToken) throw new Error('unauthorized');
+
+      const posts = await models.Post.find({
+        _id: { $in: projectSession.postList },
+      })
+        .then(d => d)
+        .catch(e => console.log('e: ', e));
+
+      return posts;
     },
     getPost: async (parent, args, { models, userSession, projectSession }) => {
       console.log('models: ', models);
       console.log('userSession: ', userSession);
       console.log('projectSession: ', projectSession);
+    },
+  },
+  Post: {
+    __resolveType: async (parent) => {
+      if (parent.assignedTo) {
+        console.log('task');
+        return 'Task';
+      }
+
+      if (parent.replies) {
+        console.log('discussion');
+        return 'Discussion';
+      }
+
+      if (parent.parentPost) {
+        console.log('comment');
+        return 'Comment';
+      }
+
+      if (parent.parentComment) {
+        console.log('reply');
+        return 'Reply';
+      }
+
+      return null;
     },
   },
 };
