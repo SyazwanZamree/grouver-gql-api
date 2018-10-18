fbf165cc4792d3d5dd5a712a3636a8a9d5a0017a
Syazwan Zamree <syazwanzamree@gmail.com>
2018-10-18 21:13:43 +0800

    unify post model. #15

---
+++ src/api/database/models/PostModel.js
@@ -0,0 +1,106 @@
+import mongoose, { Schema } from 'mongoose';
+
+const PostSchema = new Schema({
+  // interface Post
+  _id: {
+    type: Schema.Types.ObjectId,
+    auto: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  postType: {
+    type: String,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+
+  // ----------------------------
+  // shared between Task, Discussion, Comment and Reply
+
+  title: {
+    type: String,
+    required: true,
+  },
+  project: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project',
+  },
+  tags: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Tag',
+  }],
+  applause: {
+    type: Number,
+  },
+
+  // ----------------------------
+  // shared between Task, Discussion and Comment
+
+  status: {
+    type: Schema.Types.ObjectId,
+    ref: 'Status',
+  },
+
+  // ----------------------------
+  // shared between Task and Discussion
+
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+
+  // ----------------------------
+  // specific to Task
+
+  assignedTo: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  level: [{
+    type: Number,
+  }],
+  dueDate: {
+    type: Date,
+  },
+
+  // ----------------------------
+  // specific to Discussion
+
+  follower: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+
+  // ----------------------------
+  // specific to Comment
+
+  parentPost: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  }],
+  reply: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  }],
+
+  // ----------------------------
+  // specific to Reply
+
+  parentComment: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  }],
+
+  // ----------------------------
+});
+
+export default mongoose.model('Post', PostSchema);
