6d7a57ae2b197233024e4ba11302e1c52e4c81c5
Syazwan Zamree <syazwanzamree@gmail.com>
2018-10-24 09:30:27 +0800

    create mark status and follow discussion resolver. #15

--- src/api/graphql/resolvers/discussionResolvers.js
+++ src/api/graphql/resolvers/discussionResolvers.js
@@ -1,31 +1,83 @@
+import checkUserAuthentication from './utils/authentication';
+import { checkUserAuthorization } from './utils/authorization';
+
 const discussionResolvers = {
   Mutation: {
-    createDiscussion: (parent, { input }) => {
-      console.log('input: ', input);
+    markDiscussionStatus: async (parent,
+      { id, status },
+      { models, userSession, projectSession },
+    ) => {
+      const discussion = await models.Post.findById(id);
+      if (discussion.postType !== 'DISCUSSION') throw new Error('Post is not of a discussion type');
+      checkUserAuthentication(userSession, projectSession);
+      checkUserAuthorization(userSession, projectSession, discussion);
+
+      const updatedDiscussion = await models.Post.findByIdAndUpdate(
+        id,
+        { status },
+        (e) => {
+          if (e) throw new Error('cannot update comment');
+        },
+      )
+        .then(d => d)
+        .catch(e => console.log('e', e));
+
+      return updatedDiscussion;
     },
-    updateDiscussion: (parent, { input }) => {
-      console.log('input: ', input);
-    },
-    addDiscussionTag: (parent, { input }) => {
-      console.log('input: ', input);
-    },
-    markDiscussionStatus: (parent, { input }) => {
-      console.log('input: ', input);
-    },
-    followDiscussion: (parent, { input }) => {
-      console.log('input: ', input);
-    },
-    addCommentToDiscussion: async () => {
-      console.log('addCommentToTask');
-    },
-    removeDiscussion: (parent, { input }) => {
-      console.log('input: ', input);
+    followDiscussion: async (parent, { id }, { models, userSession, projectSession }) => {
+      const discussion = await models.Post.findById(id);
+      if (discussion.postType !== 'DISCUSSION') throw new Error('Post is not of a discussion type');
+
+      checkUserAuthentication(userSession, projectSession);
+      checkUserAuthorization(userSession, projectSession, discussion);
+
+      if (discussion.followers.indexOf(userSession.id) > -1) throw new Error('user already following discussion');
+
+      const updatedDiscussion = await models.Post.findByIdAndUpdate(
+        id,
+        { followers: discussion.followers.concat(userSession.id) },
+        (e) => {
+          if (e) throw new Error('cannot update task');
+        },
+      )
+        .then(d => d)
+        .catch(e => console.log('e', e));
+
+      return updatedDiscussion;
     },
   },
   Discussion: {
     createdBy: (parent, args, { models }) => {
       const user = models.User.findById(parent.createdBy);
       return user;
+    },
+    project: (parent, args, { models }) => {
+      const project = models.Project.findById(parent.project);
+      return project;
+    },
+    followers: (parent, args, { models }) => {
+      const followers = [];
+      parent.followers.forEach((e) => {
+        const user = models.User.findById(e);
+        followers.push(user);
+      });
+      return followers;
+    },
+    applaudedBy: (parent, args, { models }) => {
+      const users = [];
+      parent.applaudedBy.forEach((e) => {
+        const user = models.User.findById(e);
+        users.push(user);
+      });
+      return users;
+    },
+    comments: (parent, args, { models }) => {
+      const comments = [];
+      parent.comments.forEach((e) => {
+        const comment = models.Post.findById(e);
+        comments.push(comment);
+      });
+      return comments;
     },
   },
 };
