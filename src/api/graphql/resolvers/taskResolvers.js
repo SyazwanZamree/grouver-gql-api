3034ab7d8b9523c8abeb2922d0bd47923f6ab788
Syazwan Zamree <syazwanzamree@gmail.com>
2018-10-24 09:27:35 +0800

    import auth utils function. #15

--- src/api/graphql/resolvers/taskResolvers.js
+++ src/api/graphql/resolvers/taskResolvers.js
@@ -1,76 +1,11 @@
-function checkUserAuthentication(userSession, projectSession) {
-  if (!projectSession || userSession.invalidToken) throw new Error('unauthorized');
-  console.log('authenticated');
-}
-
-function checkUserAuthorization(userSession, projectSession, task) {
-  const usersProjectSession = JSON.stringify(userSession.projectSession);
-  const projectSessionId = JSON.stringify(projectSession.id);
-  const taskProject = JSON.stringify(task.project);
-  const isUserAuthorized = (usersProjectSession === projectSessionId)
-    && (usersProjectSession === taskProject);
-
-  if (isUserAuthorized === false && !userSession.invalidToken) throw new Error('unauthorized, not sign in');
-  console.log('signed in, authorized');
-}
-
-function checkPostCreator(userSession, task) {
-  const isUserCreator = JSON.stringify(userSession.id) === JSON.stringify(task.createdBy);
-  if (isUserCreator === false) throw new Error('user is not post creator');
-  console.log('user is post creator, authorized');
-}
+import checkUserAuthentication from './utils/authentication';
+import { checkUserAuthorization } from './utils/authorization';
 
 const taskResolvers = {
   Mutation: {
-    createTask: async (parent, { input }, { models, userSession, projectSession }) => {
-      checkUserAuthentication(userSession, projectSession);
-
-      const task = await new models.Post(input)
-        .save()
-        .then(d => d)
-        .catch(e => console.log('error', e));
-
-      task.createdBy = userSession.id;
-      task.project = projectSession.id;
-      task.applause = 0;
-      task.postType = 'TASK';
-      task.save()
-        .then(d => d)
-        .catch(e => console.log('error: ', e));
-
-      await models.Project.findByIdAndUpdate(
-        projectSession.id,
-        { postList: await projectSession.postList.concat(task.id) },
-        { new: true },
-        (e) => {
-          if (e) throw new Error('cannot update project');
-        },
-      )
-        .then(d => d)
-        .catch(e => console.log('e: ', e));
-
-      return task;
-    },
-    updateTask: async (parent, { id, input }, { models, userSession, projectSession }) => {
-      const task = await models.Post.findById(id);
-
-      checkUserAuthorization(userSession, projectSession, task);
-      checkPostCreator(userSession, task);
-
-      const updatedTask = await models.Post.findByIdAndUpdate(
-        id,
-        { $set: input },
-        (e) => {
-          if (e) throw new Error('cannot update task');
-        },
-      )
-        .then(d => d)
-        .catch(e => console.log('e', e));
-
-      return updatedTask;
-    },
     assignTaskDueDate: async (parent, { id, dueDate }, { models, userSession, projectSession }) => {
       const task = await models.Post.findById(id);
+      if (task.postType !== 'TASK') throw new Error('Post is not of a task type');
       checkUserAuthentication(userSession, projectSession);
       checkUserAuthorization(userSession, projectSession, task);
 
@@ -123,23 +58,18 @@ const taskResolvers = {
 
       return updatedTask;
     },
-    addTaskTag: async () => {
-      // TODO: restructure how tagging should work. should it be a user generated
-      // string with id in db, or enum?
-      console.log('addTaskTag');
-    },
     assignTaskToUsers: async (parent,
-      { taskInput, userInput },
+      { taskIdInput, userIdInput },
       { models, userSession, projectSession },
     ) => {
-      const task = await models.Post.findById(taskInput.id);
+      const task = await models.Post.findById(taskIdInput.id);
       checkUserAuthentication(userSession, projectSession);
       checkUserAuthorization(userSession, projectSession, task);
       const authUsers = [];
       const newUsers = [];
       const assignUsers = [];
       const users = await models.User.find({
-        _id: { $in: userInput.id },
+        _id: { $in: userIdInput.id },
       })
         .then(d => d)
         .catch(e => console.log('e: ', e));
@@ -151,12 +81,12 @@ const taskResolvers = {
       });
 
       authUsers.forEach((e) => {
-        const isNewlyAssignedTask = e.tasksAssigned.indexOf(taskInput.id);
+        const isNewlyAssignedTask = e.tasksAssigned.indexOf(taskIdInput.id);
         if (isNewlyAssignedTask <= -1) {
           newUsers.push(e.id);
           models.User.findByIdAndUpdate(
             e.id,
-            { tasksAssigned: e.tasksAssigned.concat(taskInput.id) },
+            { tasksAssigned: e.tasksAssigned.concat(taskIdInput.id) },
             { new: true },
             (er) => {
               if (er) throw new Error('cannot update user');
@@ -179,48 +109,6 @@ const taskResolvers = {
 
       return task;
     },
-    addCommentToTask: async (parent, { id, input }, { models, userSession, projectSession }) => {
-      const task = await models.Post.findById(id);
-      checkUserAuthentication(userSession, projectSession);
-      checkUserAuthorization(userSession, projectSession, task);
-
-      const comment = await new models.Post(input)
-        .save()
-        .then(d => d)
-        .catch(e => console.log('error', e));
-
-      comment.createdBy = userSession.id;
-      comment.postType = 'COMMENT';
-      comment.parentPost = task.id;
-      comment.save()
-        .then(d => d)
-        .catch(e => console.log('error', e));
-
-      models.Post.findByIdAndUpdate(
-        task.id,
-        { comments: task.comments.concat(comment.id) },
-        { new: true },
-        (e) => {
-          if (e) throw new Error('cannot update post');
-        },
-      )
-        .then(d => d)
-        .catch(er => console.log('e: ', er));
-
-      return comment;
-    },
-    removeTask: async (parent, { id }, { models, userSession, projectSession }) => {
-      const task = await models.Post.findById(id);
-
-      checkUserAuthorization(userSession, projectSession, task);
-      checkPostCreator(userSession, task);
-
-      const removedTask = await models.Post.findByIdAndRemove(id)
-        .then(d => d)
-        .catch(e => console.log('e', e));
-
-      return removedTask;
-    },
   },
   Task: {
     createdBy: (parent, args, { models }) => {
@@ -230,6 +118,14 @@ const taskResolvers = {
     project: (parent, args, { models }) => {
       const project = models.Project.findById(parent.project);
       return project;
+    },
+    assignedTo: (parent, args, { models }) => {
+      const users = [];
+      parent.assignedTo.forEach((e) => {
+        const user = models.User.findById(e);
+        users.push(user);
+      });
+      return users;
     },
     applaudedBy: (parent, args, { models }) => {
       const users = [];
@@ -246,14 +142,6 @@ const taskResolvers = {
         comments.push(comment);
       });
       return comments;
-    },
-    assignedTo: (parent, args, { models }) => {
-      const users = [];
-      parent.assignedTo.forEach((e) => {
-        const user = models.User.findById(e);
-        users.push(user);
-      });
-      return users;
     },
   },
 };
