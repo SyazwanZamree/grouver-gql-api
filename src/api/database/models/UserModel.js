1e2c9856c3d0ceaf9862608a8bdbcdd62fd06249
Syazwan Zamree <syazwanzamree@gmail.com>
2018-11-07 16:33:27 +0800

    ref badges model from users doc. #19

--- src/api/database/models/UserModel.js
+++ src/api/database/models/UserModel.js
@@ -74,6 +74,87 @@ const UserSchema = new Schema({
     default: Date.now,
     required: true,
   },
+
+  orangeBadges: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Badge',
+  }],
+
+  yellowBadges: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Badge',
+  }],
+
+  greenBadges: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Badge',
+  }],
+
+  blueBadges: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Badge',
+  }],
+
+  indigoBadges: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Badge',
+  }],
+
+  purpleBadges: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Badge',
+  }],
+
+  CRT_TASK_W: { type: Number },
+  CRT_DISCUSSION_W: { type: Number },
+  CRT_COMMENT_W: { type: Number },
+  CRT_REPLY_W: { type: Number },
+
+  CRT_TASK_M: { type: Number },
+  CRT_DISCUSSION_M: { type: Number },
+  CRT_COMMENT_M: { type: Number },
+  CRT_REPLY_M: { type: Number },
+
+  CRT_TASK_A: { type: Number },
+  CRT_DISCUSSION_A: { type: Number },
+  CRT_COMMENT_A: { type: Number },
+  CRT_REPLY_A: { type: Number },
+
+  GFL_DISCUSSION_W: { type: Number },
+  GAP_DISCUSSION_W: { type: Number },
+  GAP_TASK_W: { type: Number },
+  GAP_COMMENT_W: { type: Number },
+  GAP_REPLY_W: { type: Number },
+
+  GFL_DISCUSSION_M: { type: Number },
+  GAP_DISCUSSION_M: { type: Number },
+  GAP_TASK_M: { type: Number },
+  GAP_COMMENT_M: { type: Number },
+  GAP_REPLY_M: { type: Number },
+
+  GFL_DISCUSSION_A: { type: Number },
+  GAP_DISCUSSION_A: { type: Number },
+  GAP_TASK_A: { type: Number },
+  GAP_COMMENT_A: { type: Number },
+  GAP_REPLY_A: { type: Number },
+
+  MIP_TASK_W: { type: Number },
+  MDN_TASK_W: { type: Number },
+  MSV_DISCUSSION_W: { type: Number },
+  MSL_COMMENT_W: { type: Number },
+  MHF_COMMENT_W: { type: Number },
+
+  MIP_TASK_M: { type: Number },
+  MDN_TASK_M: { type: Number },
+  MSV_DISCUSSION_M: { type: Number },
+  MSL_COMMENT_M: { type: Number },
+  MHF_COMMENT_M: { type: Number },
+
+  MIP_TASK_A: { type: Number },
+  MDN_TASK_A: { type: Number },
+  MSV_DISCUSSION_A: { type: Number },
+  MSL_COMMENT_A: { type: Number },
+  MHF_COMMENT_A: { type: Number },
 });
 
 export default mongoose.model('User', UserSchema);
